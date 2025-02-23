42107248a365ad97c3fb6baf5aecf870fbd6993f
Laís Souza <laissouza.aalmeida@gmail.com>
2025-02-23 18:50:27 -0300

    task 2 e 3 ok

--- tasks/task-02-load-image-from-url/load_image_from_url.py
+++ tasks/task-02-load-image-from-url/load_image_from_url.py
@@ -1,6 +1,7 @@
-import argparse
 import numpy as np
-import cv2 as cv
+import cv2
+import urllib.request
+
 
 def load_image_from_url(url, **kwargs):
     """
@@ -8,16 +9,25 @@ def load_image_from_url(url, **kwargs):
     
     Parameters:
     - url (str): URL of the image.
-    - **kwargs: Additional keyword arguments for cv.imdecode (e.g., flags=cv.IMREAD_GRAYSCALE).
+    - **kwargs: Additional keyword arguments for cv.imdecode (e.g., flags=cv2.IMREAD_GRAYSCALE).
     
     Returns:
     - image: Loaded image as a NumPy array.
     """
+
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+    req = urllib.request.Request(url, headers=headers)
     
-    ### START CODE HERE ###
-    ### TODO
-    ### END CODE HERE ###
-    
+    with urllib.request.urlopen(req) as resp:
+        image_array = np.asarray(bytearray(resp.read()), dtype=np.uint8)
+
+    # Decodificar a imagem usando OpenCV
+    image = cv2.imdecode(image_array, kwargs.get('flags', cv2.IMREAD_COLOR))
+    cv2.imshow('Downloaded Image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
     return image
 
-load_image_from_url()
+
+load_image_from_url('http://answers.opencv.org/upfiles/logo_2.png')
